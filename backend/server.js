7582a8dceaceccf7b1ca0fa90ad4d25d6e26e091
Ifad Yusuf <86540168+Dhlih@users.noreply.github.com>
2025-03-14 20:08:37 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL credentials: true }));
 
 // Routes
 const userRoute = require("./routes/users");
